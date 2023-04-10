ef1ebb6a461128648d69b3690d74e1e8bf4cc3dd
Sam Lewis <Sam.Lewis@fanatics.co.uk>
2023-04-10 12:43:42 +0100

    Adding prompts and translations for integration

--- custom_components/google_trends/config_flow.py
+++ custom_components/google_trends/config_flow.py
@@ -15,9 +15,15 @@ class GoogleTrendsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="Google Trends", data=user_input)
 
         data_schema = {
-            vol.Required(CONF_COUNTRY_CODE): str,
-            vol.Required(CONF_UPDATE_INTERVAL, default=60): int,
-            vol.Required(CONF_TRENDS_COUNT, default=5): int,
+            vol.Required(
+                CONF_COUNTRY_CODE, description={"suggested_value": "united_kingdom"}
+            ): str,
+            vol.Required(
+                CONF_UPDATE_INTERVAL, default=60, description={"suggested_value": 60}
+            ): int,
+            vol.Required(
+                CONF_TRENDS_COUNT, default=5, description={"suggested_value": 5}
+            ): int,
         }
 
         return self.async_show_form(
